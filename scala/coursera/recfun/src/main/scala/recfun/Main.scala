bacec93b2fea51546f53c100690c4de54d0b596c
harleyrowland <harley.rowland@rpharms.com>
2016-11-11 13:32:52 +0000

    Complete week 1 assignment of coursera course

--- scala/coursera/recfun/src/main/scala/recfun/Main.scala
+++ scala/coursera/recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,40 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if(c <= 0) 1
+      else if(r <= 0) 1
+      else if(c >= r) 1
+      else pascal(c-1,r-1) + pascal(c,r-1)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    def balance(chars: List[Char]): Boolean = {
+      def loop(charList: List[Char], count: Int): Boolean ={
+        if(count < 0) false
+        else if(charList.isEmpty && count != 0) false
+        else if(charList.isEmpty && count == 0) true
+        else if(charList.head == '(') loop(charList.tail, count+1)
+        else if(charList.head == ')') loop(charList.tail, count-1)
+        else loop(charList.tail, count)
+      }
+      loop(chars, 0)
+    }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def loop(remainingMoneyToZero: Int, coinsLeft: List[Int]) : Int = {
+      if (coinsLeft.isEmpty) 0
+      else if (remainingMoneyToZero == 0) 1
+      else if (remainingMoneyToZero < 0) 0
+      else loop(remainingMoneyToZero - coinsLeft.head, coinsLeft) + loop(remainingMoneyToZero, coinsLeft.tail)
+    }
+
+    loop(money, coins.sorted)
   }
+}
+
